895a8171673386700324dcda112171f656f70827
Colleen <colleenhiggins87@gmail.com>
2019-06-26 11:47:27 -0700

    roulette drill added

--- react-drills-6-26/src/statedrills/RouletteGun.js
+++ react-drills-6-26/src/statedrills/RouletteGun.js
@@ -4,11 +4,16 @@ class RouletteGun extends React.Component {
   constructor (props) {
     super(props)
     this.state = {spinningTheChamber : false, chamber: null}
+    console.log(this.state)
   }
   /* spinning the chamber and pulling the trigger! ...
   you're safe!
   BANG!!!! */
+
+
   handleTrigger = ()=>{
+    console.log(this.state)
+    console.log(this.props)
 /*The state should update spinningTheChamber to be true.
 A timeout should be registered for 2 seconds.
 Within the timeout, a random number between 1 and 8 should be generated.
@@ -17,15 +22,38 @@ random number is the new value for chamber and the spinningTheChamber should be 
     this.setState(
       {spinningTheChamber:true}
     )
-    setTimeout(function(min =1, max =9){
-      const value = Math.floor(Math.random() * (max-min)+min)
+    this.timeout = setTimeout(()=>{
+      const value = Math.floor(Math.random() * (9-1)+1)
+      console.log(value);
       this.setState({spinningTheChamber: false, chamber: value})
-    }
-      ,2000)}
+    },2000)
+
+  }
+
+/*   If spinningTheChamber is true, render spinning the chamber and pulling the trigger! ....
+If the chamber value in state is the same as the bulletInChamber value in props, render BANG!!!!.
+Otherwise, render you're safe!!
+If the component is to be unmounted, clear the timeout. */
+
+handleContent = ()=> {
+  if(this.state.spinningTheChamber){
+    return 'Spinning the chamber and pulling the trigger! ....'
+  } else if(this.state.chamber === this.props.bulletInChamber) {
+    return 'BANG!!!!'
+  } else if(this.state.chamber === null){
+    return 'Try your luck!'
+  } else{
+    return `You're safe!!`
+  }
+
+}
+componentWillUnmount(){
+  clearTimeout(this.timeout);
+}
 
   render(){
     return (<div>
-      <p></p>
+      <p>{this.handleContent()}</p>
       <button onClick={this.handleTrigger}>Pull the Trigger</button>
       </div>)
   }
