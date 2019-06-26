c0e55a0e1ec04c8c4387d69b418f3a52cfa8c3e3
Colleen <colleenhiggins87@gmail.com>
2019-06-26 14:32:43 -0700

    added two last tests for accordion

--- react-drills-6-26/src/statedrills/Accordion.test.js
+++ react-drills-6-26/src/statedrills/Accordion.test.js
@@ -1,11 +1,14 @@
+import { shallow } from 'enzyme';
 import { configure } from 'enzyme';
+import toJson from 'enzyme-to-json';
 import Adapter from 'enzyme-adapter-react-16';
 import React from 'react';
-import renderer from 'react-test-renderer';
+import ReactDOM from 'react-dom';
 import Accordion from './Accordion';
 
-configure({ adapter: new Adapter() });
+configure({ adapter: new Adapter() })
 
+describe('Accordion Component', () =>{
 // The component renders an empty li when the sections prop is not supplied
 // The component renders no sections as active by default
 // The component opens a clicked section
@@ -25,11 +28,24 @@ const sections = [{
 }]
 
 it('component renders an empty li when sections prop is not supplied', () => {
-  const tree = renderer.create(<Accordion />).toJSON();
-  expect(tree).toMatchSnapshot();
+  const wrapper = shallow(<Accordion />);
+  expect(toJson(wrapper)).toMatchSnapshot();
 })
 
 it('component renders no sections as active by default', () => {
-  const tree = renderer.create(<Accordion sections={sections}/>).toJSON();
-  expect(tree).toMatchSnapshot();
+  const wrapper = shallow(<Accordion sections={sections}/>)
+  expect(toJson(wrapper)).toMatchSnapshot();
+})
+
+it('component opens a clicked section', () => {
+  const wrapper = shallow(<Accordion sections={sections} />)
+  wrapper.find('li').at(0).simulate('click', {currentTarget: 'li'})
+  expect(toJson(wrapper)).toMatchSnapshot()
+})
+it('component opens last section if multiple are clicked', () => {
+  const wrapper = shallow(<Accordion sections={sections} />)
+  wrapper.find('li').at(0).simulate('click', {currentTarget: 'li'})
+  wrapper.find('li').at(2).simulate('click', {currentTarget: 'li'})
+  expect(toJson(wrapper)).toMatchSnapshot()
+})
 })
